722fe1d78b4f887875b1a7cf96a30850eab6faf8
phalanx-hk <zfphalanx@gmail.com>
2024-01-26 01:09:51 +0900

    Add act installation script and remove apt installation script

---
+++ install/ubuntu/common/act.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# act
+echo "--- install act is start! ---"
+curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+echo "--- install act is done! ---"
